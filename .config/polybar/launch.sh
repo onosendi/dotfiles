9d0f54a7ffa01128862ae83f29d70ac3caec7cca
Daniel Lindegren <onosendi@protonmail.com>
2020-02-17 13:15:32 -0700

    Use a loop to get active monitors rather than defining the explicitly

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,9 +8,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 if [ "$(hostname)" == "desktop-arch" ]; then
-  polybar --config="$HOME/.config/polybar/config-desktop-arch" --reload DP-3 &
-  polybar --config="$HOME/.config/polybar/config-desktop-arch" --reload DP-5 &
-  polybar --config="$HOME/.config/polybar/config-desktop-arch" --reload DP-2 &
+  if type "xrandr" 2> /dev/null; then
+    for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      polybar --config="$HOME/.config/polybar/config-desktop-arch" --reload $monitor &
+    done
+  else
+    echo ERROR: xrandr not installed
+    exit 1
+  fi
 elif [ "$(hostname)" == "mbp-arch" ]; then
   polybar --config="$HOME/.config/polybar/config-mbp-arch" --reload main &
 fi
