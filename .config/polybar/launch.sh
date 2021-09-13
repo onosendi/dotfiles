75bb1aac85ce1251b9d42ddcc87fbe946f27d17a
Daniel Lindegren <onosendi@dlindegren.com>
2021-09-12 19:52:26 -0700

    style polybar with colors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,15 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-if [ "$(hostname)" == "desktop-arch" ]; then
-  if type "xrandr" 2> /dev/null; then
-    for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      polybar --config="$HOME/.config/polybar/config-desktop-arch" --reload $monitor &
-    done
-  else
-    echo ERROR: xrandr not installed
-    exit 1
-  fi
-elif [ "$(hostname)" == "mbp-arch" ]; then
-  polybar --config="$HOME/.config/polybar/config-mbp-arch" --reload main &
+
+if type "xrandr" 2> /dev/null; then
+  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    polybar --config="$HOME/.config/polybar/config" --reload $monitor &
+  done
+else
+  echo ERROR: xrandr not installed
+  exit 1
 fi
