d1256cb6f3915f09196865dc1a019110b367f341
Daniel Lindegren <onosendi@dlindegren.com>
2025-03-28 22:44:22 -0700

    Add settings for luals

--- .config/nvim/lua/config/lsp/luals.lua
+++ .config/nvim/lua/config/lsp/luals.lua
@@ -3,6 +3,23 @@ vim.lsp.config.luals = {
   filetypes = { "lua" },
   root_markers = { ".luarc.json", ".luarc.jsonc" },
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ";"),
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 
 vim.lsp.enable("luals")
