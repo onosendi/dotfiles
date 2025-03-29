65c99cacbd0754c28ad44b2928a7193a57cacc6c
Daniel Lindegren <onosendi@dlindegren.com>
2025-03-28 22:21:37 -0700

    Update jsonls filetypes with 'jsonc'

--- .config/nvim/lua/config/lsp/jsonls.lua
+++ .config/nvim/lua/config/lsp/jsonls.lua
@@ -1,6 +1,6 @@
 vim.lsp.config.jsonls = {
   cmd = { "vscode-json-language-server", "--stdio" },
-  filetypes = { "json" },
+  filetypes = { "json", "jsonc" },
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
 }
 
